bd1343a8c2438cd3807001abffc760b369a786f1
有希奈 澤田 <150584@jc-21.jp>
2017-11-14 12:32:54 +0900

    カメラ

--- FoodStock01/FoodStock01/EntryPage.xaml.cs
+++ FoodStock01/FoodStock01/EntryPage.xaml.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
 namespace FoodStock01
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -19,6 +22,10 @@ namespace FoodStock01
 
             //初期化
             InitializeComponent();
+
+            // ボタンが押された時の処理は、asyncメソッドの中で実行する必要があるので、
+            // XAMLの中にclickedは書けないんじゃないかと思うけどどうなんだろ
+            PictureButton.Clicked += takePicture;
 
         }
         void SelectSwitch(object sender, ToggledEventArgs args)
@@ -39,5 +46,41 @@ namespace FoodStock01
                 FoodPicker.IsEnabled = true;
             }
         }
+
+        async void takePicture(object sender, EventArgs e)
+        // from https://github.com/jamesmontemagno/MediaPlugin
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            StoreCameraMediaOptions cameraOption = new StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            };
+            var file = await CrossMedia.Current.TakePhotoAsync(cameraOption);
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+
+            //or:
+            //image.Source = ImageSource.FromFile(file.Path);
+            //image.Dispose();
+        }
+
     }
 }
