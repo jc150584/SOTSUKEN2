3fa3214285a220d983acf9063224f6e4dced8762
有希奈 澤田 <150584@jc-21.jp>
2017-11-22 14:26:17 +0900

    Foodページ

--- FoodStock01/FoodStock01/EntryPage.xaml.cs
+++ FoodStock01/FoodStock01/EntryPage.xaml.cs
@@ -10,6 +10,12 @@ using Xamarin.Forms.Xaml;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
 namespace FoodStock01
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -70,7 +76,7 @@ namespace FoodStock01
 
             await DisplayAlert("File Location", file.Path, "OK");
 
-            image.Source = ImageSource.FromStream(() =>
+            image.Source = Xamarin.Forms.ImageSource.FromStream(() =>
             {
                 var stream = file.GetStream();
                 file.Dispose();
@@ -80,6 +86,90 @@ namespace FoodStock01
             //or:
             //image.Source = ImageSource.FromFile(file.Path);
             //image.Dispose();
+
+
+
+        }
+
+
+
+        //登録ボタンが押された処理
+        void EntryButton_Clicked()
+        {
+
+        }
+
+        private void testcode01(MediaFile file)
+        {
+            var img = System.Drawing.Image.FromFile(file.Path);
+            BitmapData bmpData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            byte[] pixels = new byte[img.Width * img.Height * 4];
+            Marshal.Copy(bmpData.Scan0, pixels, 0, pixels.Length);
+
+            //Base64で文字列に変換
+            string base64String;
+            base64String = System.Convert.ToBase64String(bs);
+
+            //結果表示
+            EncoBase64.Text = base64String.ToString();
+        }
+
+        private void testcode03()
+        {
+            ImageConverter imgconv = new ImageConverter();
+            //imageをバイト配列に変換
+            byte[] b = (byte[])imgconv.ConvertTo(image, typeof(byte[]));
+
+            //Base64で文字列に変換
+            string base64String;
+            base64String = System.Convert.ToBase64String(b);
+
+            //結果表示
+            EncoBase64.Text = base64String.ToString();
+        }
+
+        private void testcode02(MediaFile file)
+        {
+            ////////////////////////////////
+            //delete me
+            //////////////////////////////////
+
+            Stream stream = file.GetStream();
+
+
+            //エンコード
+            int width = 128;
+            int height = width;
+            int stride = width / 8;
+            byte[] pixels = new byte[height * stride];
+
+            // イメージパレットを定義
+            BitmapPalette myPalette = BitmapPalettes.Halftone256;
+
+            // あらかじめ定義されたパレットで新しい空のイメージを作成
+            BitmapSource image2 = BitmapSource.Create(
+            width,      //幅
+            height,    //高さ
+            96,         //水平(dpi)
+            96,         //垂直(dpi)
+            PixelFormats.Indexed1, //ピクセル形式
+            myPalette,  //パレット
+            pixels,     //イメージのコンテンツを表すバイト配列
+            stride);    //stride
+
+            FileStream stream2 = new FileStream(file.Path, FileMode.Create);　      //パス名、ファイルを作成する方法
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();                    //JPEG形式のイメージのエンコーダー定義
+            TextBlock myTextBlock = new TextBlock();
+            myTextBlock.Text = "Codec Author is: " + encoder.CodecInfo.Author.ToString();
+            encoder.FlipHorizontal = true;                                          //エンコード時にイメージを水平方向に反転
+            encoder.FlipVertical = false;                                           //エンコード時にイメージを垂直方向に反転させない
+            encoder.QualityLevel = 30;                                              //品質レベル　1～100
+            encoder.Rotation = Rotation.Rotate90;                                   //イメージを回転
+            encoder.Frames.Add(BitmapFrame.Create(image2));                         //image2のフレーム設定
+            encoder.Save(stream2);                                                  //stream2にエンコード
+
+            //エンコ結果表示
+            EncoBase64.Text = encoder.ToString();
         }
 
     }
--- FoodStock01/FoodStock01/FoodPage.cs
+++ FoodStock01/FoodStock01/FoodPage.cs
@@ -1,31 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace FoodStock01
 {
-    public class FoodPage : ContentPage
+    public partial class FoodPage : ContentPage
     {
         public FoodPage(string title)
         {
-            //タブに表示される文字列
+            //タイトル
             Title = title;
 
-            //ラベルを生成
-            var label1 = new Label
-            {
-                FontSize = 40,
-                //ビューの中央に配置
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Start,
-                Text = title
-            };
+            var cell = new DataTemplate(typeof(ImageCell));
 
-            Content = label1;
+            cell.SetBinding(TextCell.TextProperty, "Name");
+            cell.SetBinding(TextCell.DetailProperty, "Days"+"日");
+            cell.SetBinding(ImageCell.ImageSourceProperty, "Image");
+
+            list.ItemTemplate = cell;
 
         }
     }
+
+
+    public class Food
+    {
+        public string Name { get; set; }
+        public string Days { get; set; }
+        //URL for our image!
+        public string Image { get; set; }
+    }
+
+
+    public class MonkeysViewModel
+    {
+        public System.Collections.ObjectModel.ObservableCollection<Food> Foods { get; set; }
+
+        public MonkeysViewModel()
+        {
+            Foods = new System.Collections.ObjectModel.ObservableCollection<Food>();
+            Foods.Add(new Food
+            {
+                Name = "ばなな",
+                Days = "3",
+                //new monkey image!
+                Image = "fruit_banana.png",
+            });
+        }
+    }
+
 }
