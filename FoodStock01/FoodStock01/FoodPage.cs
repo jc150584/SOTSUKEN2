de382c22cd2cf4fc3fae3e48782050334d7621b3
有希奈 澤田 <150584@jc-21.jp>
2017-11-22 15:14:50 +0900

    foods更新

--- FoodStock01/FoodStock01/FoodPage.cs
+++ FoodStock01/FoodStock01/FoodPage.cs
@@ -4,6 +4,10 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
+using FoodStock01.Helpers;
+using FoodStock01.Models;
+using FoodStock01.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,43 +19,21 @@ namespace FoodStock01
         {
             //タイトル
             Title = title;
-
-            var cell = new DataTemplate(typeof(ImageCell));
-
-            cell.SetBinding(TextCell.TextProperty, "Name");
-            cell.SetBinding(TextCell.DetailProperty, "Days"+"日");
-            cell.SetBinding(ImageCell.ImageSourceProperty, "Image");
-
-            list.ItemTemplate = cell;
-
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
         }
-    }
 
+        void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        => ((ListView)sender).SelectedItem = null;
 
-    public class Food
-    {
-        public string Name { get; set; }
-        public string Days { get; set; }
-        //URL for our image!
-        public string Image { get; set; }
-    }
-
-
-    public class MonkeysViewModel
-    {
-        public System.Collections.ObjectModel.ObservableCollection<Food> Foods { get; set; }
-
-        public MonkeysViewModel()
+        void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            Foods = new System.Collections.ObjectModel.ObservableCollection<Food>();
-            Foods.Add(new Food
-            {
-                Name = "ばなな",
-                Days = "3",
-                //new monkey image!
-                Image = "fruit_banana.png",
-            });
+            var monkey = ((ListView)sender).SelectedItem as Monkey;
+            if (monkey == null)
+                return;
+
+
         }
     }
-
+    }
 }
---
+++ FoodStock01/FoodStock01/Helpers/MonkeyHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodStock01.Models;
+
+namespace FoodStock01.Helpers
+{
+    public static class MonkeyHelper
+    {
+        private static Random random;
+
+        public static Monkey GetRandomMonkey()
+        {
+            //var output = Newtonsoft.Json.JsonConvert.SerializeObject(Monkeys);
+            return Monkeys[random.Next(0, Monkeys.Count)];
+        }
+
+
+        public static ObservableCollection<Grouping<string, Monkey>> MonkeysGrouped { get; set; }
+
+        public static ObservableCollection<Monkey> Monkeys { get; set; }
+
+        static MonkeyHelper()
+        {
+            random = new Random();
+            Monkeys = new ObservableCollection<Monkey>();
+            Monkeys.Add(new Monkey
+            {
+                Name = "Baboon",
+                Location = "Africa & Asia",
+                Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+                Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Capuchin Monkey",
+                Location = "Central & South America",
+                Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+                Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Blue Monkey",
+                Location = "Central and East Africa",
+                Details = "The blue monkey or diademed monkey is a species of Old World monkey native to Central and East Africa, ranging from the upper Congo River basin east to the East African Rift and south to northern Angola and Zambia",
+                Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Squirrel Monkey",
+                Location = "Central & South America",
+                Details = "The squirrel monkeys are the New World monkeys of the genus Saimiri. They are the only genus in the subfamily Saimirinae. The name of the genus Saimiri is of Tupi origin, and was also used as an English name by early researchers.",
+                Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Golden Lion Tamarin",
+                Location = "Brazil",
+                Details = "The golden lion tamarin also known as the golden marmoset, is a small New World monkey of the family Callitrichidae.",
+                Image = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg"
+            });
+
+
+
+            var sorted = from monkey in Monkeys
+                         orderby monkey.Name
+                         group monkey by monkey.NameSort into monkeyGroup
+                         select new Grouping<string, Monkey>(monkeyGroup.Key, monkeyGroup);
+
+            MonkeysGrouped = new ObservableCollection<Grouping<string, Monkey>>(sorted);
+        }
+    }
+}
