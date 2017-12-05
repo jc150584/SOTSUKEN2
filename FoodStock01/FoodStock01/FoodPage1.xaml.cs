5b2fd84476d1b794424290854516fb30adc4bef2
有希奈 澤田 <150584@jc-21.jp>
2017-12-05 16:08:40 +0900

    xamlでかいてみた

--- FoodStock01/FoodStock01/FoodPage1.xaml.cs
+++ FoodStock01/FoodStock01/FoodPage1.xaml.cs
@@ -24,7 +24,7 @@ namespace FoodStock01
 
             InitializeComponent();
 
-            var ar = new ObservableCollection<String>();
+            /*var ar = new ObservableCollection<String>();
             ar.Add(string.Format("ばなな"+"\t"+"4"+"日"+"\u2610"));
             ar.Add(string.Format("にんじん" + "\t" + "5" + "日" + "\u2610"));
             ar.Add(string.Format("とまと" + "\t" + "3" + "日" + "\u2610"));
@@ -48,9 +48,9 @@ namespace FoodStock01
 
             Content = new StackLayout
             { 
-                Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0), // iOSのみ上部にマージンをとる
+                Padding = new Thickness(20, 30, 20, 0), 
                 Children = { label1, listView }
-            };
+            };*/
         }
     }
 }
