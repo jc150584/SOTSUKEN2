29c0f74f51f65370ce908b4a19747471fdd5c7b0
有希奈 澤田 <150584@jc-21.jp>
2017-12-05 16:25:17 +0900

    もういっかい

--- FoodStock01/FoodStock01/FoodPage1.xaml.cs
+++ FoodStock01/FoodStock01/FoodPage1.xaml.cs
@@ -13,6 +13,12 @@ namespace FoodStock01
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodPage1 : ContentPage
     {
+        public ObservableCollection<Magic> Magics
+        {
+            get;
+            private set;
+        }
+
         public FoodPage1(string title)
         {
 
@@ -51,6 +57,37 @@ namespace FoodStock01
                 Padding = new Thickness(20, 30, 20, 0), 
                 Children = { label1, listView }
             };*/
+
+
+
+            Magics = new ObservableCollection<Magic> {
+                new Magic{
+                    Name = "光の白刃",
+                    Spell = "我は放つ、光の白刃"
+                },
+                new Magic{
+                    Name = "インディグネイション",
+                    Spell = "天光満る処に我は在り、黄泉の門開く処に汝在り、出でよ神の雷"
+                },
+                new Magic{
+                    Name = "ドラグスレイブ",
+                    Spell = "黄昏よりも暗き存在（もの）､血の流れよりも赤き存在（もの）時間（とき）の流れに埋もれし偉大なる汝の名において､我ここに闇に誓わん､我らが前に立ち塞がりし全ての愚かなるものに、我と汝が力もて､等しく滅びを与えんことを"
+                },
+                new Magic{
+                    Name = "セラフィックローサイト",
+                    Spell = "其は忌むべき芳名にして偽印の使徒、神苑の淵に還れ招かれざる者よ"
+                },
+                new Magic{
+                    Name = "Unlimited Blade Works",
+                    Spell = "I am the bone of my sword. Steel is my body, and fire is my blood. I have created over a thousand blades. Unknown to Death. Nor known to Life. Have withstood pain to create many weapons. Yet, those hands will never hold anything. So as I pray, unlimited blade works."
+                },
+            };
+
         }
+
+    public class Magic
+    {
+        public string Name { get; set; }
+        public string Spell { get; set; }
     }
 }
