ea0f02197df878ca9129fec756a78c7255a61de8
有希奈 澤田 <150584@jc-21.jp>
2017-12-06 15:35:27 +0900

    データサンプル変えた

--- FoodStock01/FoodStock01/FoodPageViewModel.cs
+++ FoodStock01/FoodStock01/FoodPageViewModel.cs
@@ -19,29 +19,29 @@ namespace FoodStock01
         {
             Magics = new ObservableCollection<Magic> {
                 new Magic{
-                    Name = "光の白刃",
-                    Spell = "我は放つ、光の白刃",
+                    Name = "バナナ",
+                    Date = "4",
                     Cbox = "\u2610"
                 },
                 new Magic{
-                    Name = "インディグネイション",
-                    Spell = "天光満る処に我は在り、黄泉の門開く処に汝在り、出でよ神の雷",
+                    Name = "人参",
+                    Date = "5",
                      Cbox = "\u2610"
                 },
                 new Magic{
-                    Name = "ドラグスレイブ",
-                    Spell = "黄昏よりも暗き存在（もの）､血の流れよりも赤き存在（もの）時間（とき）の流れに埋もれし偉大なる汝の名において､我ここに闇に誓わん､我らが前に立ち塞がりし全ての愚かなるものに、我と汝が力もて､等しく滅びを与えんことを",
-                     Cbox = "\u2610"
+                    Name = "トマト",
+                    Date = "7",
+                    Cbox = "\u2610"
                 },
                 new Magic{
-                    Name = "セラフィックローサイト",
-                    Spell = "其は忌むべき芳名にして偽印の使徒、神苑の淵に還れ招かれざる者よ",
-                     Cbox = "\u2610"
+                    Name = "ピーマン",
+                    Date = "3",
+                    Cbox = "\u2610"
                 },
                 new Magic{
-                    Name = "Unlimited Blade Works",
-                    Spell = "I am the bone of my sword. Steel is my body, and fire is my blood. I have created over a thousand blades. Unknown to Death. Nor known to Life. Have withstood pain to create many weapons. Yet, those hands will never hold anything. So as I pray, unlimited blade works.",
-                     Cbox = "\u2610"
+                    Name = "ほうれん草",
+                    Date = "3",
+                    Cbox = "\u2610"
                 },
             };
         }
@@ -50,7 +50,7 @@ namespace FoodStock01
     public class Magic
     {
         public string Name { get; set; }
-        public string Spell { get; set; }
+        public string Date { get; set; }
         public string Cbox { get; set; }
     }
 
