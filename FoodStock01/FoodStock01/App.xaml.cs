a2a8d483a275b80ef8d38b18e1019d09614a8fbf
有希奈 澤田 <150584@jc-21.jp>
2017-12-13 15:28:24 +0900

    もどした

--- FoodStock01/FoodStock01/App.xaml.cs
+++ FoodStock01/FoodStock01/App.xaml.cs
@@ -17,7 +17,7 @@ namespace FoodStock01
             MainPage = new TabbedPage()
             {
                 Children = {
-                   new FoodNavi(),
+                   new FoodPage1("食材"),
                    new StockPage("保存"),
                    new EntryPage1("登録"),
                    new MemoPage1("メモ"),
