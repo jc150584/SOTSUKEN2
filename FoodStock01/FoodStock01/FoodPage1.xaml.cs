c7e1672042412d518185e53101ff5f190bf54f82
有希奈 澤田 <150584@jc-21.jp>
2017-12-13 16:04:06 +0900

    動く？

--- FoodStock01/FoodStock01/FoodPage1.xaml.cs
+++ FoodStock01/FoodStock01/FoodPage1.xaml.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using FoodStock01;
@@ -12,6 +12,8 @@ namespace FoodStock01
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodPage1 : ContentPage
     {
+        ObservableCollection<ButtonAndString> myAL;
+
         public FoodPage1(string title)
         {
 
@@ -24,31 +26,56 @@ namespace FoodStock01
 
             InitializeComponent();
 
+            myAL =
+               new ObservableCollection<ButtonAndString>();
+
+            myAL.Add(new ButtonAndString { B = b1, S = b1.Text });
         }
 
-        void ChackBoxChanged(object sender, bool isChecked)
+        /*void ChackBoxChanged(object sender, bool isChecked)
         {
             //選択された時の処理
             if (isChecked)
-            {
-                
+            { 
+
+       
             }
             //選択が外された時の処理
             else
             {
                
             }
+        }*/
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+
+            String s = "見つからないよ";
+            foreach (ButtonAndString bas in myAL)
+            {
+                if ((Button)sender == (bas.B))
+                {
+                    s = bas.S;
+                }
+                DisplayAlert("ButtonAndString", s, "ok");
+            }
         }
 
-        void OnSearch_Clicked(object sender, EventArgs args)
+        public class ButtonAndString
         {
-            /*var Recipe = new WebView
+            public Button B { set; get; }
+            public String S { set; get; }
+        }
+
+        /*void OnSearch_Clicked(object sender, EventArgs args)
+        {
+            var Recipe = new WebView
             {
                 Source = "http://cookpad.com/search/"
             };
-            Content = Recipe;*/
+            Content = Recipe;
 
-            //Application.Current.MainPage = new RecipePage("レシピ");
-        }
+            Application.Current.MainPage = new RecipePage("レシピ");
+        }*/
     }
 }
