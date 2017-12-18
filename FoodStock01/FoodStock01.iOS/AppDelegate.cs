953eabcbcebc950d9b62ffc965fe258fe1432fa4
有希奈 澤田 <150584@jc-21.jp>
2017-12-18 16:31:01 +0900

    AppDelegate追加

--- FoodStock01/FoodStock01.iOS/AppDelegate.cs
+++ FoodStock01/FoodStock01.iOS/AppDelegate.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using Foundation;
+using UIKit;
 using System.Collections.Generic;
 using System.Linq;
 
+namespace FoodStock01.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            //指定したファイルのパスを取得する。
+            var dbPath = GetLocalFilePath("culculate.db3");
+
+            //この段階ではまだエラーになる。
+            LoadApplication(new App(dbPath));
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        public static string GetLocalFilePath(string filename)
+        {
+            //指定されたファイルのパスを取得する。なければ作成してそのパスを返却する。
+            var docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var libFolder = System.IO.Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!System.IO.Directory.Exists(libFolder))
+            {
+                System.IO.Directory.CreateDirectory(libFolder);
+            }
+
+            return System.IO.Path.Combine(libFolder, filename);
+        }
+    }
+}
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
 using Foundation;
 using UIKit;
-
 namespace FoodStock01.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -24,8 +62,7 @@ namespace FoodStock01.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
             return base.FinishedLaunching(app, options);
         }
     }
-}
+}*/
