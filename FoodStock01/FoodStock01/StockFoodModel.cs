f4dc7e7d1117b146ccf3a43a9591236c4e27f411
有希奈 澤田 <150584@jc-21.jp>
2017-12-19 12:37:26 +0900

    コピペ

---
+++ FoodStock01/FoodStock01/StockFoodModel.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace FoodStock01
+{
+    [Table("Stock")]//テーブル名を指定
+    public class StockFoodModel
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int S_no { get; set; } //保存食材No
+
+        public string S_name { get; set; } //保存食材名
+
+        public int S_num { get; set; } //数量
+
+        public string S_unit { get; set; } //単位
+
+        /********************インサートメソッド**********************/
+        public static void InsertStock(int s_no, string s_name, int s_num, string s_unit)
+        {
+            //データベースに接続する
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースにStockテーブルを作成する
+                    db.CreateTable<StockFoodModel>();
+
+                    db.Insert(new StockFoodModel() { S_no = s_no, S_name = s_name, S_num = s_num, S_unit = s_unit });
+                    db.Commit();
+                }
+                catch (Exception e)
+                {
+                    db.Rollback();
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+        }
+
+        /*******************セレクトメソッド**************************************/
+        public static List<StockFoodModel> SelectStock()
+        {
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースに指定したSQLを発行
+                    return db.Query<StockFoodModel>("SELECT * FROM [Stock] ORDER BY [S_num]");
+
+                }
+                catch (Exception e)
+                {
+
+                    System.Diagnostics.Debug.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+
+        /********************デリートメソッド*************************************/
+        public static void DeleteStock(int s_no)
+        {
+            //データベースに接続する
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースにFoodテーブルを作成する
+                    db.CreateTable<StockFoodModel>();
+
+                    db.Delete<StockFoodModel>(s_no);//デリートで渡す値は主キーじゃないといけない説
+                    db.Commit();
+                }
+                catch (Exception e)
+                {
+                    db.Rollback();
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+        }
+
+        /********************オールデリートメソッド*************************************/
+        public static void DeleteAllStock()
+        {
+            //データベースに接続する
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースにFoodテーブルを作成する
+                    db.CreateTable<StockFoodModel>();
+
+                    db.DeleteAll<StockFoodModel>();
+                    db.Commit();
+                }
+                catch (Exception e)
+                {
+                    db.Rollback();
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+        }
+
+        /********************アップデートメソッド（プラス）**************************************/
+        public static List<StockFoodModel> UpdateStockPlus(int s_no)
+        {
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースに指定したSQLを発行
+                    return db.Query<StockFoodModel>("UPDATE [Stock] SET [S_num] = [S_num] + 1 WHERE [S_no] = 1");
+
+                }
+                catch (Exception e)
+                {
+
+                    System.Diagnostics.Debug.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+
+        /********************アップデートメソッド02（プラス試し）**************************************/
+        public static void UpdateStockPlus02(int s_no)
+        {
+            //データベースに接続する
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースにFoodテーブルを作成する
+                    db.CreateTable<StockFoodModel>();
+
+                    db.Delete<StockFoodModel>(s_no);//デリートで渡す値は主キーじゃないといけない説
+                    db.Commit();
+                }
+                catch (Exception e)
+                {
+                    db.Rollback();
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+        }
+
+        /********************アップデートメソッド（マイナス）**************************************/
+        public static List<StockFoodModel> UpdateStockMinus(int s_no)
+        {
+            using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
+            {
+                try
+                {
+                    //データベースに指定したSQLを発行
+                    return db.Query<StockFoodModel>("UPDATE [Stock] SET [S_num] = [S_num] - 1 WHERE [S_no] = " + s_no);
+
+                }
+                catch (Exception e)
+                {
+
+                    System.Diagnostics.Debug.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+    }
+}
--- FoodStock01/FoodStock01/StockPage1.xaml.cs
+++ FoodStock01/FoodStock01/StockPage1.xaml.cs
@@ -9,23 +9,42 @@ using Xamarin.Forms.Xaml;
 
 namespace FoodStock01
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class StockPage1 : ContentPage
-	{
-		public StockPage1 (string title)
-		{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StockPage1 : ContentPage
+    {
+        public StockPage1(string title)
+        {
             //タブに表示される文字列
             Title = title;
 
-            InitializeComponent ();
-		}
+            InitializeComponent();
+        }
+
+        //更新ボタンが押された時
+        private void Update_Button_Clicked(object sender, EventArgs e)
+        {
+            Title = "保存";
+
+            InitializeComponent();
+
+        }
 
         //プラスがクリックされた
         void OnPlus_Clicked(object sender, EventArgs args)
         {
             int num = Convert.ToInt32(((CustomButton)sender).CountText) + 1;
-            string name = ((CustomButton)sender).NameText;
-            DisplayAlert("1足しました", name+"　"+num.ToString(), "ok");
+            string no = ((CustomButton)sender).NameText;
+
+            int s_no = int.Parse(no);
+            DisplayAlert("1足しました", "主キー" +s_no+ "　個数" + num.ToString(), "ok");
+            //
+            /***ここから試し***/
+            StockFoodModel.UpdateStockPlus(s_no);
+
+            Title = "保存";
+
+            InitializeComponent();
+            /***ここまで試し***/
         }
 
         //マイナスがクリックされた
@@ -33,7 +52,7 @@ namespace FoodStock01
         {
             int num = Convert.ToInt32(((CustomButtonMinus)sender).CountText) - 1;
             string name = ((CustomButtonMinus)sender).NameText;
-            DisplayAlert("1引きました", name+"　"+num.ToString(), "ok");
+            DisplayAlert("1引きました", name + "　" + num.ToString(), "ok");
         }
     }
 }
