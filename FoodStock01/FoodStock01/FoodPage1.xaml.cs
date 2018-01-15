777c603a8c2a9c4f2f1b204b73847cb156b5a2bb
有希奈 澤田 <150584@jc-21.jp>
2018-01-15 12:45:38 +0900

    もどした

--- FoodStock01/FoodStock01/FoodPage1.xaml.cs
+++ FoodStock01/FoodStock01/FoodPage1.xaml.cs
@@ -38,30 +38,6 @@ namespace FoodStock01
             }
         }
 
-        /// <summary>
-        /// コードビハインドにItemTappedイベント発火時のコードを書く必要がある
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //タップで削除できるかな
-        async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-             string no = ((CustomButtonDelete)sender).NoText;
-            string name = ((CustomButtonDelete)sender).NameText;
-
-            var result = await DisplayAlert("削除", "この食材を削除しますか", "OK", "キャンセル");
-            if (result == true)
-            {
-                int f_no = int.Parse(no);
-                FoodModel.DeleteFood(f_no);
-
-                Title = "食材リスト";
-                s = "http://cookpad.com/search/";
-
-                InitializeComponent();
-            }
-        }
-
         //デリート押された
         //void OnDelete_Clicked(object sender, EventArgs args)
         async void OnDelete_Clicked(object sender, EventArgs args)
