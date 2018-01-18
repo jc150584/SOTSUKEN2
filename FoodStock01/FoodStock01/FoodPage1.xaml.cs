b2294fbb0b076094678941d1a8eccfabab916851
有希奈 澤田 <150584@jc-21.jp>
2018-01-18 10:34:52 +0900

    初回

--- FoodStock01/FoodStock01/FoodPage1.xaml.cs
+++ FoodStock01/FoodStock01/FoodPage1.xaml.cs
@@ -15,6 +15,10 @@ namespace FoodStock01
     {
         String s = "http://cookpad.com/search/";
 
+        ContentView qsl; // iOS/Android 用
+        ContentView bl; // 黒背景
+        bool qslVisible = true;
+
         public FoodPage1(string title)
         {
             //タイトル
@@ -22,6 +26,91 @@ namespace FoodStock01
 
             InitializeComponent();
 
+            ToolbarItems.Add(new ToolbarItem
+            {
+                Text = "検索",
+                Icon = "search24.png",
+                Command = new Command(() =>
+                    //ページ遷移
+                    Navigation.PushAsync(new NextPage(s))
+                ),
+            });
+
+            AbsoluteLayout abs = new AbsoluteLayout { };
+
+            // 上に表示される View です。
+            // 黒背景を最初に置いて、
+            bl = new ContentView
+            {
+                BackgroundColor = Color.Black,
+                Opacity = 0.65d,
+            };
+
+            // 更に上に View を被せました。
+            qsl = new ContentView
+            {
+                Content = new StackLayout
+                {
+                    Children = {
+                        // 画像で誤魔化します
+                        new Image {
+                            Source = "QuickStart.png",
+                            WidthRequest = 250,
+                            HorizontalOptions = LayoutOptions.End,
+                        },
+                        new Image {
+                            Source = "QuickStartSwipe.png",
+                            WidthRequest = 340,
+                        },
+                        // 閉じると Application Properties に bool 値を保存します。
+                        new Button {
+                            Text = "閉じる",
+                            VerticalOptions = LayoutOptions.EndAndExpand,
+                            Command = new Command (()=>{
+                                qsl.IsVisible = false;
+                                bl.IsVisible = false;
+                                qslVisible = false;
+                                Application.Current.Properties["qsl"] = qslVisible;
+                            }),
+                        },
+                    }
+                }
+            };
+
+            // AbsoluteLayout にコントロールを追加しますがその際に
+            // Properties Dictionary をチェックして QuickStart Layer を追加しています。
+            abs.Children.Add(ml);
+            if (Application.Current.Properties.ContainsKey("qsl"))
+            {
+                var bqsl = (bool)Application.Current.Properties["qsl"];
+                if (bqsl)
+                    abs.Children.Add(qsl);
+            }
+            else
+            {
+                abs.Children.Add(qsl);
+            }
+
+            Title = "QuickStartLayer";
+            Content = abs;
+
+            SizeChanged += OnPageSizeChanged;
+        }
+        /// <summary>
+        /// 画面サイズ変更時(起動して画面が表示される時)に呼び出されます。この後でないと画面サイズが取得できません。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void OnPageSizeChanged(object sender, EventArgs args)
+        {
+            var w = this.Width;
+            var h = this.Height;
+
+            AbsoluteLayout.SetLayoutFlags(ml, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(ml, new Rectangle(0d, 0d, w, h));
+
+            AbsoluteLayout.SetLayoutFlags(qsl, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(qsl, new Rectangle(0d, 0d, w, h));
         }
 
         void ChackBoxChanged(object sender, bool isChecked)
@@ -45,15 +134,6 @@ namespace FoodStock01
             string no = ((CustomButtonDelete)sender).NoText;
             string name = ((CustomButtonDelete)sender).NameText;
 
-            /*
-            DisplayAlert("Delete","主キー"+no+" "+name,"OK");
-            
-            int f_no = int.Parse(no);
-            FoodModel.DeleteFood(f_no);
-            Title = "食材リスト";
-            s = "http://cookpad.com/search/";
-            InitializeComponent();
-            */
             var result = await DisplayAlert("削除", "この食材を削除しますか", "OK", "キャンセル");
             if (result == true)
             {
